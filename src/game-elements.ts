b34c48c31ef25195a123c3619c7114fc0af67d32
Ibrahn Sahir <ibrahn.sahir@gmail.com>
2024-03-12 11:24:02 +0000

    Dither fruit.

--- src/game-elements.ts
+++ src/game-elements.ts
@@ -4,7 +4,7 @@ enum GameElements {
     SnakeBody   = 0x20,
     SnakeHead   = 0x40,
     SnakeChomp  = 0x60,
-    Fruit       = 0x80,
+    Fruit       = 0x82,
     Wall        = 0xd0,
     WallHeavy   = 0xc8,
     WallMedium  = 0xe2,
